a19ac3025005458f905a49fe89885c5f4e55cb3e
Tauriq <tauriqh786@gmail.com>
2021-10-26 15:26:07 +0200

    th: completed notes-card-component

--- src/app/components/note-card/note-card.component.ts
+++ src/app/components/note-card/note-card.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  OnInit,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-note-card',
   templateUrl: './note-card.component.html',
-  styleUrls: ['./note-card.component.scss']
+  styleUrls: ['./note-card.component.scss'],
 })
-export class NoteCardComponent implements OnInit {
+export class NoteCardComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  @Input() title: string;
+  @Input() body: string;
+
+  @ViewChild('truncator') truncator: ElementRef<HTMLElement>;
+  @ViewChild('bodyText') bodyText: ElementRef<HTMLElement>;
+
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit(): void {
+
   }
 
+  ngAfterViewInit(): void {
+    // do this in after view init, other wise the viewchilds will be undefined
+    // work out if there is a text overflow and if not, then hide the truncator
+
+    let style = window.getComputedStyle(this.bodyText.nativeElement, null);
+    let viewableHeight = parseInt(style.getPropertyValue('height'), 10);
+
+    if (this.bodyText.nativeElement.scrollHeight > viewableHeight) {
+      // if there is a text overflow, show the fade out truncator
+      this.renderer.setStyle(this.truncator.nativeElement, 'display', 'block');
+    } else {
+      // else there is no textoverflow, hide fad eout truncator
+      this.renderer.setStyle(this.truncator.nativeElement, 'display', 'none');
+    }
+  }
 }
--- src/app/pages/notes-list/notes-list.component.ts
+++ src/app/pages/notes-list/notes-list.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotesListComponent implements OnInit {
 
+  title: string;
+  body: string;
+
   constructor() { }
 
   ngOnInit(): void {
