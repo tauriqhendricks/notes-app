0f662198ca1003d0ed76d2e0188306eb0c16befb
Tauriq <tauriqh786@gmail.com>
2021-11-02 16:10:16 +0200

    th: added search list feature

--- src/app/pages/notes-list/notes-list.component.ts
+++ src/app/pages/notes-list/notes-list.component.ts
@@ -84,15 +84,65 @@ import { NotesService } from 'src/app/shared/services/notes.service';
 export class NotesListComponent implements OnInit {
 
   notes: Note[] = [];
+  filteredNotes: Note[] = [];
 
   constructor(private notesService: NotesService) { }
 
   ngOnInit(): void {
     this.notes = this.notesService.getAll();
+    this.filteredNotes = this.notes;
   }
 
   onDelete(id: number): void {
     this.notesService.delete(id);
+  }
+
+  onFilter(event: Event): void {
+    let query = (event.target as HTMLInputElement).value;
+    query = query.toLowerCase().trim();
+
+    // split up query into idividual words
+    let terms: string[] = query.split(' ');
+
+    // remove duplicate search terms
+    terms = this.removeDuplicate<string>(terms);
+
+    // compilem all relevent results
+    let allResults: Note[] = []
+    terms.forEach(term => {
+      let results = this.releventNotes(term);
+      // merge the 2 lists
+      allResults = [...allResults, ...results];
+    });
+
+    // allResults can include duplicate notes
+    // bc a particular note can be the result of many search terms
+    // we don't want to display the same note multiple times
+    const uniqueResults = this.removeDuplicate<Note>(allResults);
+    this.filteredNotes = uniqueResults;
+  }
+
+  removeDuplicate<T>(list: T[]): T[] {
+    // sets don't have duplicate values
+    let uniqueResults: Set<any> = new Set();
+
+    // loop thru list, and items to set
+    list.forEach(x => uniqueResults.add(x));
+
+    return Array.from(uniqueResults);
+  }
+
+  releventNotes(query: string): Note[] {
+    query = query.toLowerCase().trim();
+
+    const releventNotes = this.notes.filter(note => {
+      if (note.title.toLowerCase().includes(query) || note.body?.toLowerCase().includes(query))
+        return true;
+      else
+        return false;
+    });
+
+    return releventNotes;
   }
 
 }
